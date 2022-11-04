f979aab883d72a06701a77511e6f91df13399e2d
Gabriela <105010227+GabriAizawa@users.noreply.github.com>
2022-11-03 23:05:35 -0500

    2 Commit
    Se logró abrir el Chrome

--- Prueba_1.py
+++ Prueba_1.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-driver = webdriver.Chrome('C:/Selenium/chromedriver')
+driver = webdriver.Chrome('C:/Users/riuni/OneDrive/Escritorio/Portafolio de las materias/Portafolio de Mantenimiento & Pruebas/4. Actividades Desarrolladas durante el curso/3. Laboratorios/Lab 9/chromedriver.exe')
 driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
 
 time.sleep(100)
-driver.quite()
+driver.close()
