9a3ada40098a7f7a950377314a9c03435c7f9e4a
airlenys21 <105010041+airlenys21@users.noreply.github.com>
2022-11-04 00:55:10 -0500

    creacion de código prueba
    hasta selección de articulo

--- Prueba_1.py
+++ Prueba_1.py
@@ -6,5 +6,20 @@ driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
 
+barradebusqueda = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "field-keywords")))
+time.sleep(2)
+
+busqueda = ("HP Pavilon azul")
+barradebusqueda.send_keys(busqueda)
+time.sleep(2)
+
+lupa = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "nav-search-submit-button")))
+lupa.click()
+time.sleep(2)
+
+articulo = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div/div[2]/div/div/div[1]/h2/a/span")))
+articulo.click()
+time.sleep(2)
+
 time.sleep(100)
 driver.close()
