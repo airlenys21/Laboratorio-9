178426e09de49f59a7b6bd39b71d7f1116d73028
airlenys21 <105010041+airlenys21@users.noreply.github.com>
2022-11-04 00:32:32 -0500

    Agregamos driver

--- Prueba_1.py
+++ Prueba_1.py
@@ -1,10 +1,13 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 import time
-driver = webdriver.Chrome('C:/Selenium/chromedriver')
+driver = webdriver.Chrome('C:/Users/Airlenys/OneDrive - Universidad Tecnológica de Panamá/Documentos/II Semestre/MANTENIMIENTO Y PRUEBAS DE SOFTWARE/Laboratorio-9/chromedriver.exe')
 driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
 
+
 time.sleep(100)
-driver.close()
+driver.quit()
